819e51dab0dd463be5e7f403273f262fb4c9b543
rioalifian12 <rioalifian12@gmail.com>
2021-10-08 14:56:45 +0700

    Telah menyelesaikan video Struktur Folder, Routes & View

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rio Alifian Santoso",
+        "email" => "rioalifian12@yahoo.com",
+        "image" => "rio.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
