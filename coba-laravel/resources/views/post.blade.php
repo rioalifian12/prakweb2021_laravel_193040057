442af2930c7c579fc161cb5696d6a5c2e863300c
rioalifian12 <rioalifian12@gmail.com>
2021-10-14 21:02:37 +0700

    Telah menyelesaikan video Post Category & Eloquent Relationship

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.main')
 @section('container')
 <h1 class="mb-5">{{ $post->title }}</h1>
+<p>By. Sandhika Galih in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 {!! $post->body !!}
 
 <a href="/blog">Back to Posts</a>
