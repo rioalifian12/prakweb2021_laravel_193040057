4e587a3d396255d300483c82b2ef189dcdea1c2a
rioalifian12 <rioalifian12@gmail.com>
2021-11-18 12:18:01 +0700

    Telah menyelesaikan video Dashboard UI

--- coba-laravel/resources/views/partials/navbar.blade.php
+++ coba-laravel/resources/views/partials/navbar.blade.php
@@ -35,6 +35,8 @@
               <form action="/logout" method="post">
                 @csrf
                 <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+              </form>
+            </li>
           </ul>
         </li>
         @else
